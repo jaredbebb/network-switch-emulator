0378123a5cb9c20ef8ddf97c955c7be3b8327eb8
jaredbebb <j.bebb@live.com>
2023-01-02 12:33:46 -0500

    refacocted ptp message into class

--- interfaces_send.py
+++ interfaces_send.py
@@ -3,17 +3,23 @@
 # scapy==2.4.5
 ####
 
+from ptp_messages import PTPAnnounce
+
+from scapy import interfaces
 from scapy.all import *
-from ptp_messages import ptp_announce
 
-# IFACES.show() # let’s see what interfaces are available. Windows only
-iface = "Intel(R) Ethernet Connection I218-LM"#<<"full iface name">> or <<IFACES.dev_from_index(12)>> or <<IFACES.dev_from_pcapname(r"\\Device_stuff")>>
-socket = conf.L2socket(iface=iface)
-# socket is now an Ethernet socket
+# print available network interfaces
+interfaces.show_interfaces()
 
+ethernet_interface = "Intel(R) Ethernet Connection I218-LM"
+
+# create Ethernet socket
+socket = conf.L2socket(iface=ethernet_interface)
 
 ether_layer = b'\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\xff\x88\xf7'
-pkt = ether_layer + ptp_announce
+
+ptp_announce = PTPAnnounce()
+pkt = ether_layer + ptp_announce.pack_message()
 raw_pkt = Raw(pkt)
 print(raw_pkt)
 
--- ptp_messages.py
+++ ptp_messages.py
@@ -1,34 +1,44 @@
-ptp_messageType = b'\x0b'
-ptp_versionPtp = b'\x02'
-ptp_messageLength = b'\x00\x40'
-ptp_domainNumber = b'\x00'
-ptp_minorSdoID = b'\x00'
-ptp_LI_61 = b'\x00\x04'
-ptp_CorrectionField = b'\x00\x00\x00\x00\x00\x00\x00\x00'
-ptp_messageTypeSpecific = b'\x00\x00\x00\x00'
-ptp_clockIdentity = b'\x00\x0d\xa8\xff\xfe\xf16\xf17\xb1'
-ptp_surcePortID = b'\x00\x0a'
-ptp_sequenceID = b'\x01\x89'
-ptp_controlField = b'\x05'
-ptp_logMessagePeriod = b'\xfd'
-ptp_originTimeStampSeconds = b'\x00\x00\x63\x60\x3a\xa6'
-ptp_originTimeStampNanoSeconds = b'\x29\x70\xa7\xf4'
-ptp_originCurrentUTCOffset = b'\x00\x25'
-ptp_PrecisionTimeProtocol = b'\x00'
-ptp_priority = b'\x01'
-ptp_grandmasterClockClass = b'\x06'
-ptp_grandmasterClockAccuracy = b'\x20'
-ptp_grandmasterClockVariance = b'\xff\xff'
-ptp_priority2 = b'\x01'
-ptp_grandmasterClockIdentity = b'\x00\x0d\xa8\xff\xfe\x16\x17\xb1'
-ptp_localStepsRemoved = b'\x00\x00'
-ptp_TimeSource = b'\x20'
+class PTPMessage():
+    def __init__(self):
+        self.messageType = b'\x0b'
+        self.versionPtp = b'\x02'
+        self.messageLength = b'\x00\x40'
+        self.domainNumber = b'\x00'
+        self.minorSdoID = b'\x00'
+        self.LI_61 = b'\x00\x04'
+        self.CorrectionField = b'\x00\x00\x00\x00\x00\x00\x00\x00'
+        self.messageTypeSpecific = b'\x00\x00\x00\x00'
+        self.clockIdentity = b'\x00\x0d\xa8\xff\xfe\xf16\xf17\xb1'
+        self.surcePortID = b'\x00\x0a'
+        self.sequenceID = b'\x01\x89'
+        self.controlField = b'\x05'
+        self.logMessagePeriod = b'\xfd'
+        self.originTimeStampSeconds = b'\x00\x00\x63\x60\x3a\xa6'
+        self.originTimeStampNanoSeconds = b'\x29\x70\xa7\xf4'
+        self.originCurrentUTCOffset = b'\x00\x25'
+        self.PrecisionTimeProtocol = b'\x00'
+        self.priority = b'\x01'
+        self.grandmasterClockClass = b'\x06'
+        self.grandmasterClockAccuracy = b'\x20'
+        self.grandmasterClockVariance = b'\xff\xff'
+        self.priority2 = b'\x01'
+        self.grandmasterClockIdentity = b'\x00\x0d\xa8\xff\xfe\x16\x17\xb1'
+        self.localStepsRemoved = b'\x00\x00'
+        self.TimeSource = b'\x20'
 
-# TODO: this should be packed struct based on HW architecture endianness see
-# https://docs.python.org/3/library/struct.html#byte-order-size-and-alignment
-ptp_announce = ptp_messageType + ptp_versionPtp + ptp_messageLength + ptp_domainNumber + ptp_minorSdoID + \
-    ptp_LI_61 + ptp_CorrectionField + ptp_messageTypeSpecific + ptp_clockIdentity + ptp_surcePortID + \
-    ptp_sequenceID + ptp_controlField + ptp_logMessagePeriod + ptp_originTimeStampSeconds + \
-    ptp_originTimeStampNanoSeconds + ptp_originCurrentUTCOffset + ptp_PrecisionTimeProtocol + ptp_priority + \
-    ptp_grandmasterClockClass + ptp_grandmasterClockAccuracy + ptp_grandmasterClockVariance + ptp_priority2 + \
-    ptp_grandmasterClockIdentity + ptp_localStepsRemoved + ptp_TimeSource
+    def pack_message(self):
+        # TODO: this should be packed struct based on HW architecture endianness see
+        # https://docs.python.org/3/library/struct.html#byte-order-size-and-alignment
+        message = self.messageType + self.versionPtp + self.messageLength + self.domainNumber + self.minorSdoID + \
+            self.LI_61 + self.CorrectionField + self.messageTypeSpecific + self.clockIdentity + self.surcePortID + \
+            self.sequenceID + self.controlField + self.logMessagePeriod + self.originTimeStampSeconds + \
+            self.originTimeStampNanoSeconds + self.originCurrentUTCOffset + self.PrecisionTimeProtocol + self.priority + \
+            self.grandmasterClockClass + self.grandmasterClockAccuracy + self.grandmasterClockVariance + self.priority2 + \
+            self.grandmasterClockIdentity + self.localStepsRemoved + self.TimeSource
+        return message
+
+
+class PTPAnnounce(PTPMessage):
+    def __init__(self):
+        super().__init__()
+        self.messageType = b'\x0b'
